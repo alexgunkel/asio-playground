1c161316545a63f36fe405872b0c515f587bcc58
Alexander Gunkel <alexander.gunkel@check24.de>
2021-11-12 10:18:56 +0100

    play with timers

--- main.cpp
+++ main.cpp
@@ -3,38 +3,43 @@
 
 class Ticker {
 public:
-    explicit Ticker(asio::io_service &ioService) : timer{ioService, asio::chrono::milliseconds {200}} {
-        tick();
+    explicit Ticker(asio::io_service &ioService) : strand_{asio::make_strand(ioService)},
+                                                   timer{ioService, asio::chrono::milliseconds{20}},
+                                                   timerCopy{ioService, asio::chrono::milliseconds{20}} {
+        tick(timer);
+        tick(timerCopy);
     };
-    void tick()
-    {
+
+    void tick(asio::steady_timer &t) {
         std::cout << "class " << count_ << "\n";
-        count_ += 200;
-        if (count_ < 10'000) {
-            timer.expires_after(asio::chrono::milliseconds{200});
-            timer.async_wait([this](const asio::error_code& ec) {
+        count_ += 20;
+        if (count_ < 20'000) {
+            t.expires_after(asio::chrono::milliseconds{20});
+            auto cb = asio::bind_executor(strand_, [this, &t](const asio::error_code &ec) {
                 if (!ec) {
-                    tick();
+                    tick(t);
                 }
             });
+            t.async_wait(std::move(cb));
         }
     }
 
 private:
+    asio::strand<asio::io_service::executor_type> strand_;
     asio::steady_timer timer;
+    asio::steady_timer timerCopy;
     int count_{};
 };
 
-void print(const asio::error_code& ec, const std::shared_ptr<asio::steady_timer>& timer, int* counter)
-{
+void print(const asio::error_code &ec, const std::shared_ptr<asio::steady_timer> &timer, int *counter) {
     std::cout << (*counter) << "\n";
     (*counter) += 290;
 
     if (*counter > 10'000)
         return;
 
-    timer->expires_after(asio::chrono::milliseconds {290});
-    timer->async_wait([=](const asio::error_code& ec) {
+    timer->expires_after(asio::chrono::milliseconds{290});
+    timer->async_wait([=](const asio::error_code &ec) {
         print(ec, timer, counter);
     });
 }
@@ -48,6 +53,10 @@ int main() {
     print(asio::error_code{}, t, &c);
     new Ticker{ioService};
     std::cout << "end\n";
+    asio::thread th{[&]() {
+        ioService.run();
+    }};
     ioService.run();
+    th.join();
     return 0;
 }
